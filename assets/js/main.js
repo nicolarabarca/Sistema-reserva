ce5629b92cbea097da7cede0578be837e6028fe8
ni.colara@hotmail.com <ni.colara@hotmail.com>
2022-02-13 17:29:56 -0300

    se agrega flujo para validar pase de movilidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+/* Función ciclo While pregunta si usuario tiene pase de movilidad para hacer reserva
+ ya que sin el no se puede ingresar al local.*/
+
+function validateMobilityPass() {
+  let repeat = true;
+  let askMobilityPass = prompt(
+    "¿Deseas continuar con la reserva. Responde con un SI o un NO"
+  ).toLocaleLowerCase();
+  while (repeat) {
+    if (askMobilityPass == "si") {
+      alert("Puedes continuar con tu reserva :D");
+      break;
+    } else if (askMobilityPass == "no") {
+      alert("Conseguir mi pase de movilidad");
+      location.href =
+        "https://www.chileatiende.gob.cl/fichas/93514-pase-de-movilidad";
+      repeat = false;
+    }
+  }
+}
+
+validateMobilityPass();
+
 // id boton eviar  datos para hacer confirmaciòn
 const buttonSend = document.getElementById("send");
 buttonSend.addEventListener("click", post, false);
@@ -36,7 +59,6 @@ function post() {
 }
 //Array para almacenar datos solicitados al usuario para reserva
 let personDataBase = [];
-
 // Se guardan e imprimen datos en pantalla para luego pedir confimaciòn a usuario y finalizar la reserva
 function addData() {
   personDataBase.push(captureUser);
@@ -59,15 +81,15 @@ function addData() {
     "</p>";
 }
 
-//Se solicita id de formulario para acceder a check terminos y condiciones
+//CICLO donde se solicita id de formulario para acceder a check terminos y condiciones
 
 const validateTerms = (document.getElementById("applicationForm").onsubmit =
   function acceptTerms(event) {
     accept = document.applicationForm.terms; //acceso a check terminos y condiciones
-
+    event.preventDefault();
     if (accept.checked == true) {
-      addData(event);
-      event.preventDefault();
+      addData();
+      cleanInput();
     } else {
       //Ingresa en esta parte del ciclo cuando se hace click en boton enviar sin aceptar terminos
       alert(
@@ -76,3 +98,8 @@ const validateTerms = (document.getElementById("applicationForm").onsubmit =
       return false; // no se envia informaciòn de reserva
     }
   });
+// Limpiar campos cuando el usuario haga click en enviarpara que no se  duplique  informaciòn
+function cleanInput() {
+  setTimeout("document.applicationForm.reset()", 100);
+  return false;
+}
